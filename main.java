34d0d1da473bdd5210ec170e784c240eaf05b653
Nagiller <felix.nagiller@yahoo.de>
2024-12-31 09:00:05 +0100

    esBeginntLangsam

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args){
-        System.out.println("Hallo Welt!");
+        //System.out.println("Hallo Welt!");
+
+        /* Ich habe das alles nach einem Videotutorial auf YouTube mitgecodet und eigentlich nur immer soweit
+        voraus geschrieben, wie ich es selbst konnte. Das heißt, dass es nicht meine Lokik ist.
+         */
     }
 }
